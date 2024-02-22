83d039ed57580b7b47c0b991338f85ddd5364bc5
arahman01 <abdulrahmanmehr19@gmail.com>
2024-02-23 03:36:49 +0500

    add social links to hero section

--- utility/texts.js
+++ utility/texts.js
@@ -2,3 +2,7 @@ export const landingPageHeading =
   "I'm Abdul Rahman, welcome to my corner of the web!";
 export const landingPageIntro =
   "A passionate software engineer based in Lahore, Pakistan, dedicated to crafting innovative digital solutions. With over two years of experience, I specialize in developing web applications that seamlessly bridge business needs with cutting-edge technology";
+
+// LINKS
+export const linkedinLink = "https://www.linkedin.com/in/abdul-rahman-mehr";
+export const githubLink = "https://github.com/arahman01";
