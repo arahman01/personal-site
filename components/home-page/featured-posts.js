5a3c8eea2f43e8ce8ba7596fcb3c94b3c09a5061
arahman01 <abdulrahmanmehr19@gmail.com>
2024-02-24 03:58:13 +0500

    add view all posts button on featured posts

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,12 +1,22 @@
-import PostGrid from "../posts/posts-grid";
+import { useRouter } from "next/router";
 
+import PostGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
 
 function FeaturedPosts(props) {
+  const router = useRouter();
+
+  const handleViewAllPostsClick = () => {
+    router.push("/posts");
+  };
+
   return (
     <section className={classes.latest}>
       <h2>Featured Posts</h2>
       <PostGrid posts={props.posts} />
+      <div className={classes.actions}>
+        <button onClick={handleViewAllPostsClick}>View All Posts</button>
+      </div>
     </section>
   );
 }
