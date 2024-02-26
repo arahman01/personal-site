b8fd3d55890caff67ddc08b56d02092f46c67982
arahman01 <abdulrahmanmehr19@gmail.com>
2024-02-26 22:45:03 +0500

    aboutme boilerplate

---
+++ components/about-me/education.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+import classes from "./about-me.module.css";
+
+const Education = () => {
+  return (
+    <Paper elevation={3} className={classes.section}>
+      <Typography variant="h5" gutterBottom>
+        Education
+      </Typography>
+      <Paper elevation={0} className={classes.education}>
+        <Typography variant="body1">
+          Bachelor of Science in Computer Science - Your University (Year)
+        </Typography>
+      </Paper>
+      <Paper elevation={0} className={classes.education}>
+        <Typography variant="body1">
+          Certifications in Web Development - Issuing Organization (Year)
+        </Typography>
+      </Paper>
+      {/* Add more education details as needed */}
+    </Paper>
+  );
+};
+
+export default Education;
---
+++ components/about-me/skills.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+import { Code } from "@mui/icons-material";
+import classes from "./about-me.module.css";
+
+const Skills = () => {
+  return (
+    <Paper elevation={3} className={classes.section}>
+      <Typography variant="h5" gutterBottom>
+        Skills
+      </Typography>
+      <div className={classes.skill}>
+        <Code />
+        <Typography variant="body1">
+          Proficient in JavaScript, React, HTML, CSS
+        </Typography>
+      </div>
+      <div className={classes.skill}>
+        <Code />
+        <Typography variant="body1">
+          Familiar with Node.js, Express.js, MongoDB
+        </Typography>
+      </div>
+      {/* Add more skills as needed */}
+    </Paper>
+  );
+};
+
+export default Skills;
