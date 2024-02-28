f5e934e7191b60db674d4edac015e41522d0c454
arahman01 <abdulrahmanmehr19@gmail.com>
2024-02-28 22:28:09 +0500

    about me section headings

--- components/education/education.js
+++ components/education/education.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Paper, Typography } from "@mui/material";
 import classes from "./education.module.css";
+import commonStyles from "../../styles/common.module.css";
 
 const Education = () => {
   return (
-    <Paper elevation={3} className={classes.section}>
-      <Typography variant="h5" gutterBottom>
-        Education
-      </Typography>
-      <Paper elevation={0} className={classes.education}>
-        <Typography variant="body1">
-          Bachelor of Science in Computer Science - Your University (Year)
+    <Fragment>
+      <h1 className={commonStyles.pageSectionHeading}>Education</h1>
+      <Paper elevation={3} className={classes.section}>
+        <Typography variant="h5" gutterBottom>
+          Education
         </Typography>
+        <Paper elevation={0} className={classes.education}>
+          <Typography variant="body1">
+            Bachelor of Science in Computer Science - Your University (Year)
+          </Typography>
+        </Paper>
+        <Paper elevation={0} className={classes.education}>
+          <Typography variant="body1">
+            Certifications in Web Development - Issuing Organization (Year)
+          </Typography>
+        </Paper>
+        {/* Add more education details as needed */}
       </Paper>
-      <Paper elevation={0} className={classes.education}>
-        <Typography variant="body1">
-          Certifications in Web Development - Issuing Organization (Year)
-        </Typography>
-      </Paper>
-      {/* Add more education details as needed */}
-    </Paper>
+    </Fragment>
   );
 };
 
--- components/skills/skills.js
+++ components/skills/skills.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Paper, Typography } from "@mui/material";
 import { Code } from "@mui/icons-material";
 import classes from "./skills.module.css";
+import commonStyles from "../../styles/common.module.css";
 
 const Skills = () => {
   return (
-    <Paper elevation={3} className={classes.section}>
-      <Typography variant="h5" gutterBottom>
-        Skills
-      </Typography>
-      <div className={classes.skill}>
-        <Code />
-        <Typography variant="body1">
-          Proficient in JavaScript, React, HTML, CSS
+    <Fragment>
+      <h1 className={commonStyles.pageSectionHeading}>
+        Software Engineer Spotlight
+      </h1>
+      <Paper elevation={3} className={classes.section}>
+        <Typography variant="h5" gutterBottom>
+          Skills
         </Typography>
-      </div>
-      <div className={classes.skill}>
-        <Code />
-        <Typography variant="body1">
-          Familiar with Node.js, Express.js, MongoDB
-        </Typography>
-      </div>
-      {/* Add more skills as needed */}
-    </Paper>
+        <div className={classes.skill}>
+          <Code />
+          <Typography variant="body1">
+            Proficient in JavaScript, React, HTML, CSS
+          </Typography>
+        </div>
+        <div className={classes.skill}>
+          <Code />
+          <Typography variant="body1">
+            Familiar with Node.js, Express.js, MongoDB
+          </Typography>
+        </div>
+        {/* Add more skills as needed */}
+      </Paper>
+    </Fragment>
   );
 };
 
