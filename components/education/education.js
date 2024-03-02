b038b94689b11f5818e60440eee394f57fe2eba7
arahman01 <abdulrahmanmehr19@gmail.com>
2024-03-02 06:08:48 +0500

    modal integration in education section

--- components/education/education.js
+++ components/education/education.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import classes from "./education.module.css";
 import commonStyles from "../../styles/common.module.css";
-
+import Modal from "../modal/modal";
 import EducationList from "./educationList";
 
-const professionalDevelopmentData = [
+const formalEducationData = [
   {
     logo: "/Udemy-Logo.png",
     institution: "Information Technology University",
@@ -27,7 +27,101 @@ const professionalDevelopmentData = [
   },
 ];
 
+const coursesAndCertificatesData = [
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "random1",
+    degree: "course 1",
+    dates: "2018 - 2022",
+    certificateLink: " ",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "snfjn fjsdnfl",
+    degree: "dnflgjkbskf",
+    dates: "2016 - 2018",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "dsgfnf sdjnbfjofn fdas f",
+    degree: "dfngjsodbniof",
+    dates: "2013 - 2016",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "random1",
+    degree: "course 123",
+    dates: "2018 - 2022",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "sabc sdnfl",
+    degree: "dnflgjkbskf",
+    dates: "2016 - 2018",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "dsgfjofn fdas f",
+    degree: "dfngjsodbniof",
+    dates: "2013 - 2016",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "random1",
+    degree: "course 1",
+    dates: "2018 - 2022",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "snfbf dnfl",
+    degree: "dfjdeb f",
+    dates: "2016 - 2018",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "ds32845rsdbf ofn fdas f",
+    degree: "dfngjsodbniof",
+    dates: "2013 - 2016",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "random193845u",
+    degree: "course ejfb1",
+    dates: "2018 - 2022",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "snfjn fjsdnfl",
+    degree: "dnqqqbskf",
+    dates: "2016 - 2018",
+    explanation: "High School",
+  },
+  {
+    logo: "/Udemy-Logo.png",
+    institution: "jenbtljbdgfls f",
+    degree: "dfngjsodefnj ajbrdbniof",
+    dates: "2013 - 2016",
+    explanation: "High School",
+  },
+];
+
 const Education = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Fragment>
       <h1 className={commonStyles.pageSectionHeading}>Education</h1>
@@ -49,16 +143,33 @@ const Education = () => {
       <div className={classes.container}>
         <div className={classes.flexContainer}>
           <div className={classes.educationContainerOne}>
-            <EducationList
-              data={professionalDevelopmentData}
-              isPaginated={false}
-            />
+            <EducationList data={formalEducationData} isPaginated={false} />
           </div>
           <div className={classes.modalButton}>
-            <button>Courses & Certificates</button>
+            <button onClick={openModal}>Courses & Certificates</button>
           </div>
         </div>
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        modalContentStyle={classes.modal}
+      >
+        <div className={classes.modalHeadings}>
+          <h2>Courses & Certificates</h2>
+          <h3>
+            In my quest for growth, I regularly pursue courses and
+            certifications to enhance my professional skills
+          </h3>
+        </div>
+        <div className={classes.modalList}>
+          <EducationList
+            data={coursesAndCertificatesData}
+            isPaginated={true}
+            pageSize={3}
+          />
+        </div>
+      </Modal>
     </Fragment>
   );
 };
