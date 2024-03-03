120401b11824abe3a1908b025c5b6058d8090b3e
arahman01 <abdulrahmanmehr19@gmail.com>
2024-03-03 19:37:16 +0500

    fixes

--- components/experience/experience.js
+++ components/experience/experience.js
@@ -9,23 +9,36 @@ function Experience(props) {
   return (
     <Fragment>
       <h1 className={commonStyles.pageSectionHeading}>Experience</h1>
-      <Paper elevation={0} className={styles.root}>
+      <Paper
+        elevation={0}
+        sx={{
+          backgroundColor: "transparent",
+          background: "none",
+          padding: "var(--size-4) 0",
+        }}
+      >
         {experiences.map((experience, index) => (
           <div key={index} className={styles.experienceContainer}>
             <div className={styles.experience}>
-              <Typography variant="h6" className={styles.fontStyles}>
+              <Typography variant="h6" sx={{ fontFamily: "var(--font-var)" }}>
                 {experience.title}
               </Typography>
-              <Typography variant="subtitle1" className={styles.fontStyles}>
+              <Typography
+                variant="subtitle1"
+                sx={{ fontFamily: "var(--font-var)" }}
+              >
                 {experience.company}
               </Typography>
-              <Typography variant="subtitle2" className={styles.fontStyles}>
+              <Typography
+                variant="subtitle2"
+                sx={{ fontFamily: "var(--font-var)" }}
+              >
                 {experience.duration}
               </Typography>
               <Typography
                 variant="body1"
                 component="div"
-                className={styles.fontStyles}
+                sx={{ fontFamily: "var(--font-var)" }}
               >
                 <ul>
                   {experience.description.split("\n").map((item, i) => (
