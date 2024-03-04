6e5a3b2c1b5680f59faeadfc8c9ceb35fbfb559e
arahman01 <abdulrahmanmehr19@gmail.com>
2024-03-05 00:35:07 +0500

    config mongo

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,20 +1,20 @@
 // api/contact
 
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { email, name, message } = req.body;
 
     if (
       !email ||
-      !email.includes('@') ||
+      !email.includes("@") ||
       !name ||
-      name.trim() === '' ||
+      name.trim() === "" ||
       !message ||
-      message.trim() === ''
+      message.trim() === ""
     ) {
-      res.status(422).json({ message: 'Invalid input.' });
+      res.status(422).json({ message: "Invalid input." });
       return;
     }
 
@@ -28,21 +28,23 @@ async function handler(req, res) {
 
     try {
       client = await MongoClient.connect(
-        'mongodb+srv://abdulrahman:bullfox.dam2@cluster0.siufxlm.mongodb.net/my-site?retryWrites=true&w=majority'
+        `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.siufxlm.mongodb.net/my-site?retryWrites=true&w=majority`
       );
     } catch (error) {
-      res.status(500).json({ message: 'Could not connect to database.' });
+      res.status(500).json({
+        message: "Could not send the message. Please try again later",
+      });
       return;
     }
 
     const db = client.db();
 
     try {
-      const result = await db.collection('messages').insertOne(newMessage);
+      const result = await db.collection("messages").insertOne(newMessage);
       newMessage.id = result.insertedId;
     } catch (error) {
       client.close();
-      res.status(500).json({ message: 'Storing message failed!' });
+      res.status(500).json({ message: "Storing message failed!" });
       return;
     }
 
@@ -50,7 +52,7 @@ async function handler(req, res) {
 
     res
       .status(201)
-      .json({ message: 'Successfully stored message!', message: newMessage });
+      .json({ message: "Successfully stored message!", message: newMessage });
   }
 }
 
